ad69f40d2d72b52ff3e5268f799604bc0a8d0efc
Glenn Forrest <glennjamesforrest@gmail.com>
2015-08-25 04:49:17 +0000

    Added button functionality to load GameState

--- menu.js
+++ menu.js
@@ -5,14 +5,14 @@ var menuState = {
         
         var titleLabel = game.add.text(80, 80, 'Platformer', 
                                         {font:'50px Arial', fill:'#ffffff'});
-        var titleLabel = game.add.text(80, 80, 'press the "up" key to start', 
+        var instructionsLabel = game.add.text(80, 80, 'press the "up" key to start', 
                                         {font:'25px Arial', fill:'#ffffff'});                                
 
-        var upKey = cursor.up;
+        var wKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
 
-        upKey.onDown.addOnce(this.start, this);
+        wKey.onDown.addOnce(this.start, this);
     },
     start: function () {
-            //game.state.start('game');
+            game.state.start('game');
     }
 };
