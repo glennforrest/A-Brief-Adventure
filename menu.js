65374c34ae4d53e6b23a77362d448ad142e31999
Glenn Forrest <glennjamesforrest@gmail.com>
2015-08-25 09:12:40 +0000

    menuState styled, music added

--- menu.js
+++ menu.js
@@ -1,15 +1,23 @@
+/* global game */
+/* global Phaser */
+
 var menuState = {
     create: function () {
-        // Here we display the menu items, so text as well as any image needed. Background image the
-        // same as the loader?
         
-        var titleLabel = game.add.text(80, 80, 'Platformer', 
-                                        {font:'50px Arial', fill:'#ffffff'});
-        var instructionsLabel = game.add.text(80, 80, 'press the "up" key to start', 
-                                        {font:'25px Arial', fill:'#ffffff'});                                
+        // Displaying Background and Text
+        game.add.sprite(0, 0, 'space');
+        game.add.text(80, 80, 'Platformer', {font:'50px Arial', fill:'#ffffff'} );
+        game.add.text(80, 150, 'press the "up" key to start', {font:'25px Arial', fill:'#ffffff'} );                                
 
+
+        // Play intro music
+        var music = game.add.audio('gameMusic');
+        music.play();
+        
+        // Mapping the Up key
         var wKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-
+        
+        // Event Listener calling the Start function to begin the game
         wKey.onDown.addOnce(this.start, this);
     },
     start: function () {
