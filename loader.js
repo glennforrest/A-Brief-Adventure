49a3123cd8d84557d015548a4836a14a8868599f
Glenn Forrest <glennjamesforrest@gmail.com>
2015-08-25 05:41:33 +0000

    all assets loaded

--- loader.js
+++ loader.js
@@ -1,20 +1,56 @@
+/* global game */
+
 var loaderState = {
     preload: function () {
         
-        
+        game.add.sprite(0, 0, 'space');
         // Adding a loading text onto the screen
         var loadingLabel = game.add.text(80, 150, 'loading...', 
                                         {font: '30px Courier', fill: '#ffffff'});
         
         
-        // load all game assets
-        // images, spritesheets, atlases, audio etc..
-        game.load.tilemap('myTilemap', 'assets/tilemaps/scifi.json', null, Phaser.Tilemap.TILED_JSON);
-        game.load.image('myTileset', "assets/tilemaps/scifi_platformTiles_32x32.png");
-        game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-        game.load.spritesheet('skeleton', 'assets/skeleton_3.png', 64, 64);
-        game.load.image('bg', 'assets/scifi_platform_BG1.jpg');
-        game.load.image('treasure', 'assets/star2.png');
+        /**
+         * Game art loading
+         **/ 
+         
+        // Sprites
+        game.load.spritesheet('dude', '/assets/images/player.png', 32, 48);
+        game.load.spritesheet('enemy', '/assets/images/enemy.png', 32, 32);
+            
+        // Backgrounds
+        game.load.image('snow', '/assets/images/snow.png');
+        
+        // Platforms
+        game.load.image('platform', '/assets/images/platform.png');
+        game.load.image('cloud', '/assets/images/cloud-platform.png');
+        
+        // Buttons
+        game.load.image('musicIcon', '/assets/images/music-icon.png');
+        game.load.image('SFXIcon', '/assets/images/SFX-icon.png');
+        
+        // Miscellaneous
+        game.load.image('briefcase', '/assets/images/briefcase.png');
+        
+        
+        /**
+         * Game audio loading
+         **/ 
+        
+        // SFX
+        game.load.audio('step','assets/sound/footstep.wav');
+        game.load.audio('step','assets/sound/jump.wav');
+        game.load.audio('step','assets/sound/lava-drip.wav');
+        game.load.audio('step','assets/sound/lava-splash.mp3');
+        game.load.audio('step','assets/sound/sizzle.wav');
+        game.load.audio('step','assets/sound/thud.wav');
+        
+        //Music
+        game.load.audio('step','assets/sound/game-music.mp3');
+        game.load.audio('step','assets/sound/intro-music.mp3');
+        game.load.audio('step','assets/sound/victort-music.mp3');
+        
+        
+        
     },
     create: function () {
         //call next state - change game to MainMenu when development complete
