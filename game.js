7ea880ee18474a9db69ba43b493aa2be1c4edc1a
Glenn Forrest <glennjamesforrest@gmail.com>
2015-08-25 11:15:41 +0000

    Music playing and looping

--- game.js
+++ game.js
@@ -1,17 +1,31 @@
+/* global game */
+
 var gameState = {
     
     
     preload: function(){
-    
+    // Check the states tutorial to see what needs to be inserted into this function here. Otherwise I might not need it?
     var background = game.add.tileSprite(0, 0, 800, 600, 'snow');    
-    //var background = game.add.sprite(0, 0, 'snow');
-   
+    
+    
     },
     
     
     create: function(){
+        // Add in all assets
         
+        // Game art
         
+        // Sounds
+        
+        this.music = game.add.audio('gameMusic');
+        this.music.play();
+        
+    },
+    update: function(){
+         if(!this.music.isPlaying){
+            this.music.play();
+        }  
     }
     
     
