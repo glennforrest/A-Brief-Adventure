dadac3c9b7d32903828fb4cb9ffea1258f4181f2
Glenn Forrest <glennjamesforrest@gmail.com>
2015-08-28 00:35:27 +0000

    Moved assets

--- js/boot.js
+++ js/boot.js
@@ -16,6 +16,20 @@ var bootState = {
         // Starting the physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        if (this.game.device.desktop) {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            //this.scale.setMinMax(800, 600, 1024, 768);
+        //    this.scale.pageAlignHorizontally = true;
+         //   this.scale.pageAlignVertically = true;
+        } else {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            //this.scale.setMinMax(480, 260, 1024, 768);
+            this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+            this.scale.forceOrientation(true, false);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+        }
         // Calling the load state
         game.state.start('loader');
     }
