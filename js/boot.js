6640d90c3fa2d0c0ba1e4f3f78bc81d492b33320
Glenn Forrest <glennjamesforrest@gmail.com>
2015-08-31 08:05:09 +0000

    Added orientation image on Portrait orientation

--- js/boot.js
+++ js/boot.js
@@ -8,6 +8,7 @@ var bootState = {
         game.load.image('space', '/assets/images/space.png');
         game.load.image('loadingBar', '/assets/images/loading-bar.png');
         game.load.image('loadingBarContainer', '/assets/images/loading-bar-container.png');
+        game.load.image('orientation', 'orientation.png');
         // Load the stars background
         // Load the sprite for the loading bar
     },
@@ -17,8 +18,21 @@ var bootState = {
         // Starting the physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        
+        if (!this.game.device.desktop) {
+           
+          this.scale.forceOrientation(true, false, 'orientation');
+          this.scale.enterIncorrectOrientation.add(this.rescale, this);
+        }
+
         // Calling the load state
         game.state.start('loader');
-    }
+    },
+    rescale: function() {
+
+    // the game doesn't resize right away, so we need to delay here 
+    var _this = this;
+    setTimeout(function() {
+      _this.scale.orientationSprite.scale.set(_this.game.width / _this.scale.width, _this.game.height / _this.scale.height) }, 100);
+    
+  }
 };
--- js/menu.js
+++ js/menu.js
@@ -4,12 +4,14 @@
 var menuState = {
     init: function(){
         if (this.game.device.desktop){
-            this.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.input.onDown.add(this.gofull, this);
+            //this.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
+            //this.input.onDown.add(this.gofull, this);
         }
         else{
-            this.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+            this.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
             this.input.onDown.add(this.gofull, this);
+            this.input.onDown.add(this.start, this);
+            
         }
         
         
@@ -32,7 +34,15 @@ var menuState = {
         // Displaying Background and Text
         game.add.sprite(0, 0, 'space');
         game.add.text(80, 80, 'A Brief Adventure', {font:'50px Arial', fill:'#ffffff'} );
-        game.add.text(80, 150, 'press the "up" key to start', {font:'25px Arial', fill:'#ffffff'} );                                
+        if(game.device.desktop){
+            game.add.text(80, 150, 'press the "up" key to start', {font:'25px Arial', fill:'#ffffff'} );
+        }else{
+            game.add.text(80, 150, 'touch the screen to start', {font:'25px Arial', fill:'#ffffff'} );
+            game.add.text(80, 225, 'walk right: touch the right side of the screen', {font:'25px Arial', fill:'#ffffff'} );
+            game.add.text(80, 260, 'walk left: touch the left side of the screen', {font:'25px Arial', fill:'#ffffff'} );
+            game.add.text(80, 295, 'jump: swipe up on the screen', {font:'25px Arial', fill:'#ffffff'} );
+            
+        }
         this.musicIcon = game.add.image(700, 20, 'musicIcon');
         this.SFXIcon = game.add.image(750, 20, 'SFXIcon');
         // Enables all kind of input actions on this image (click, etc)
@@ -58,9 +68,7 @@ var menuState = {
         
         // Event Listener calling the Start function to begin the game
         wKey.onDown.addOnce(this.start, this);
-        game.input.onTap.add(function(){
-            this.start();
-        });
+        
     },
     toggleMusic: function() {
         this.musicIcon.alpha = 1;
